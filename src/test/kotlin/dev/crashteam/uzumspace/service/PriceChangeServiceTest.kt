7d545223e5ab06affc15f48e4a2835811aecdfa8
vitaxa <vitaxa93gamebox@gmail.com>
2023-12-19 23:10:09 +0300

    fix filter shop items (fix test) [2]

--- src/test/kotlin/dev/crashteam/uzumspace/service/PriceChangeServiceTest.kt
+++ src/test/kotlin/dev/crashteam/uzumspace/service/PriceChangeServiceTest.kt
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDateTime
 import java.util.*
 
 @Testcontainers
+@SpringBootTest
 class PriceChangeServiceTest : ContainerConfiguration() {
 
     @Autowired
