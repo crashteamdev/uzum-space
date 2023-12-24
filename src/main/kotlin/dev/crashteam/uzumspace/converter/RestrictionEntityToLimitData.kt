035cb049ac682940e8932ba1adc2e599c7933bc5
SageFreke <lenigrast@gmail.com>
2023-12-24 14:21:48 +0300

    fix limit data

--- src/main/kotlin/dev/crashteam/uzumspace/converter/RestrictionEntityToLimitData.kt
+++ src/main/kotlin/dev/crashteam/uzumspace/converter/RestrictionEntityToLimitData.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component
 class RestrictionEntityToLimitData : DataConverter<RestrictionEntity, LimitData> {
     override fun convert(source: RestrictionEntity): LimitData? {
         return LimitData().apply {
-            id = source.userId
+            userId = source.userId
             uzumAccountLimit = source.uzumAccountLimit.toLong()
             uzumAccountLimitCurrent = source.uzumAccountLimitCurrent.toLong()
             itemPoolLimit = source.itemPoolLimit.toLong()
