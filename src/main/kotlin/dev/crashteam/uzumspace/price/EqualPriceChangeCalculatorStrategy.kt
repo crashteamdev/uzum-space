5e151c798478b988e15f1620e79418f6f1a2261f
SageFreke <lenigrast@gmail.com>
2024-03-13 20:38:21 +0300

    fixed equal price strategy (not changing price if equals to current)

--- src/main/kotlin/dev/crashteam/uzumspace/price/EqualPriceChangeCalculatorStrategy.kt
+++ src/main/kotlin/dev/crashteam/uzumspace/price/EqualPriceChangeCalculatorStrategy.kt
@@ -6,8 +6,11 @@ import dev.crashteam.uzumspace.repository.postgre.UzumAccountShopItemCompetitorR
 import dev.crashteam.uzumspace.repository.postgre.entity.UzumAccountShopItemCompetitorEntity
 import dev.crashteam.uzumspace.service.UzumShopItemService
 import dev.crashteam.uzumspace.service.model.ShopItemCompetitor
+import mu.KotlinLogging
 import java.math.BigDecimal
 import java.util.*
+
+private val log = KotlinLogging.logger {}
 
 class EqualPriceChangeCalculatorStrategy(
     private val uzumAccountShopItemCompetitorRepository: UzumAccountShopItemCompetitorRepository,
@@ -34,26 +37,23 @@ class EqualPriceChangeCalculatorStrategy(
         val competitorPrice: BigDecimal = uzumShopItemService.getRecentPrice(minimalPriceCompetitor.shopItemEntity)!!
         val competitorPriceMinor = competitorPrice.movePointRight(2)
 
+        var newPriceMinor: BigDecimal? = null
         if ((options?.minimumThreshold != null && options.maximumThreshold != null)
             && (competitorPriceMinor >= BigDecimal.valueOf(options.minimumThreshold)
                     && competitorPriceMinor <= BigDecimal.valueOf(options.maximumThreshold))
         ) {
+            newPriceMinor = competitorPriceMinor
+        } else if (options?.minimumThreshold != null && competitorPriceMinor < BigDecimal.valueOf(options.minimumThreshold)) {
+            newPriceMinor = BigDecimal.valueOf(options.minimumThreshold)
+        } else if (options?.maximumThreshold != null && competitorPriceMinor > BigDecimal.valueOf(options.maximumThreshold)) {
+            newPriceMinor = BigDecimal.valueOf(options.maximumThreshold)
+        }
+        if (newPriceMinor != null && newPriceMinor.compareTo(sellPriceMinor) == 0) {
+            log.info { "New price $newPriceMinor equal to current price for shop item $uzumAccountShopItemId" }
+            return null
+        } else if (newPriceMinor != null) {
             return CalculationResult(
-                newPriceMinor = competitorPriceMinor,
-                competitorId = minimalPriceCompetitor.competitorEntity.id
-            )
-        } else if (options?.minimumThreshold != null && competitorPriceMinor < BigDecimal.valueOf(options.minimumThreshold)
-            && sellPriceMinor != BigDecimal.valueOf(options.minimumThreshold)
-        ) {
-            return CalculationResult(
-                newPriceMinor = BigDecimal.valueOf(options.minimumThreshold),
-                competitorId = minimalPriceCompetitor.competitorEntity.id
-            )
-        } else if (options?.maximumThreshold != null && competitorPriceMinor > BigDecimal.valueOf(options.maximumThreshold)
-            && sellPriceMinor != BigDecimal.valueOf(options.maximumThreshold)
-        ) {
-            return CalculationResult(
-                newPriceMinor = BigDecimal.valueOf(options.maximumThreshold),
+                newPriceMinor = newPriceMinor,
                 competitorId = minimalPriceCompetitor.competitorEntity.id
             )
         }
